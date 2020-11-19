f76321982419e8af19948e75f7d039cbbcf0afa9
AanviKothari456 <73425604+AanviKothari456@users.noreply.github.com>
2020-11-19 18:55:03 +0530

    Update Hexagon.js

--- Hexagon.js
+++ Hexagon.js
@@ -10,6 +10,7 @@ class Hexagon{
         this.body = Bodies.circle(x, y, radius, options);
         this.radius = radius;
         this.image = loadImage("download.png");
+        this.Visibility = 255;
         World.add(world, this.body);
 
     }
@@ -22,5 +23,17 @@ class Hexagon{
         fill("red");
         image(this.image, 0, 0,this.radius, this.radius);
         pop();
+        console.log(this.body);
+    if(this.body.speed<5){
+    super.display();
+    }else{
+      World.remove(world,this.body);
+      push();
+      this.Visibility = this.Visibility - 5;
+    tint(255, this.Visibility);
+    image(this.image, this.body.position.x,this.body.position.y,50,50);
+    pop();
+    }
+  }
     }
 }
